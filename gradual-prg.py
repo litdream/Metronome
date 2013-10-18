c1eadef90098f5e1a221102828295be0d519b7d4
Raymond Chung <rchung725@gmail.com>
2013-10-18 10:44:54 -0400

    Flexible change for --bump-up parameter.

--- gradual-prg.py
+++ gradual-prg.py
@@ -3,19 +3,41 @@
 import os
 import sys
 import time
+import getopt
 
-prgfile=sys.argv[1]
+bump_up = 0
+
+sa = "hU:"
+la = ('help', 'bump-up=')
+o,argv = getopt.getopt(sys.argv[1:], sa, la)
+
+def usage():    
+    print '''\
+USAGE: %s [options]  DAT_FILE
+  -h, --help             help screen
+  -U, --bump-up=INT      bump up speed
+''' % sys.argv[0]
+
+if len(o) + len(argv) == 0:  usage(); sys.exit(1)
+for k,v in o:
+    if k in ('-h','--help'):
+        usage(); sys.exit(0)
+    if k in ('-U','--bump-up'):
+        bump_up = int(v)
+
+prgfile=argv[0]
 for line in open(prgfile):
     line = line.strip()
     if len(line) == 0: continue
 
     working, dura, rest = tuple(line.split())
-    os.system("python Metronome.py -t %s --gradual-dura=%s" % (working, dura))
+    cur_speed = int(working) + bump_up
+    os.system("python Metronome.py -t %d --gradual-dura=%s" % (cur_speed, dura))
     print '''
 
-WORKING : %s
+WORKING : %d
 
-''' % working
+''' % cur_speed
     os.system("mplayer a.wav > /dev/null")
     
     time.sleep( int(rest))
